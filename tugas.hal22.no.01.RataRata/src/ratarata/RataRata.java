4c7fc3015a6e244f6e1a422b5a3fd78f35b83ac9
Axce29 <Axce29@DESKTOP-BMJEUCT>
2018-10-06 21:22:52 +0700

    Mencari nilai rata - rata

---
+++ tugas.hal22.no.01.RataRata/src/ratarata/RataRata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ratarata;
+
+/**
+ *
+ * @author Axce29
+ */
+public class RataRata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int numb1 = 10;
+    int numb2 = 20;
+    int numb3 = 45;
+    int rataRata;
+    rataRata = (numb1 + numb2 + numb3)/3;
+    
+        System.out.println(" number 1 = " + numb1);
+        System.out.println(" number 2 = " + numb2);
+        System.out.println(" number 3 = " + numb3);
+        System.out.println(" Rata - rata = " + rataRata);
+    }
+    
+}
